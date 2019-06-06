f705ccb4f2787137b5d1652a86f9faa80d2cd911
xyn11 <xuyinan11@gmail.com>
2019-06-05 17:13:26 -0700

    -add right script

--- script.py
+++ script.py
@@ -4,18 +4,29 @@ import csv
 
 def scrape_wensite(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content,features="lxml")
-    tmp = []
-    price_box = soup.find('HistoricalPriceStore', attrs= {'class' : 'prices'})
-    price = price_box.text
-    tmp.append(price)
-    return tmp
+    soup = BeautifulSoup(response.content,'html.parser')
+    records = []
+    tables = list(soup.find_all('table', attrs={'class':"rwd-table"}))
+    head = []
+    hds = tables[0].find_all('th')
+    for hd in hds:
+            head.append(hd)
+    for t in tables:
+        tbody = t.find('tbody')
+        rows = list(tbody.find_all('tr'))
+        for r in rows:
+                tds = r.find_all('td')
+                re = []
+                for td in tds:
+                        re.append(td.text)
+                if len(re) == len(head):
+                        records.append(re)
+    return records
 
-def output():
+def output(records):
     with open('output.csv', 'a') as f:
         writer = csv.writer(f)
-        writer.writerow()
+        writer.writerow(records)
 
-url = 'https://finance.yahoo.com/quote/%5EIXIC/history?p=%5EIXIC'
-s = scrape_wensite(url)
-print(s)
+url = 'http://www.finra.org/investors/margin-statistics'
+records = scrape_wensite(url)
