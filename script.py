dfc6c09f565f6491405eac90a201d5e612b26453
xyn11 <xuyinan11@gmail.com>
2019-06-12 15:10:03 -0700

    -split into 2 files

--- script.py
+++ script.py
@@ -1,75 +1,60 @@
 from bs4 import BeautifulSoup
 import requests
-import matplotlib.pyplot as plt
 import csv
 
 def getdata(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content,'html.parser')
-    tables = list(soup.find_all('table', attrs={'class':"rwd-table"}))
-    head = []
-    hds = tables[0].find_all('th')
-    for hd in hds:
-            head.append(hd.text)
-    records = []
-    for t in tables:
-        tbody = t.find('tbody')
-        rows = list(tbody.find_all('tr'))
-        for r in rows:
-                tds = r.find_all('td')
-                re = []
-                for td in tds:
-                        re.append(td.text)
-                if len(re) == len(head):
-                        records.append(re)
-    return (head, records)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content,'html.parser')
+        tables = list(soup.find_all('table', attrs={'class':"rwd-table"}))
+        # get head of table
+        head = []
+        hds = tables[0].find_all('th')
+        for hd in hds:
+                head.append(hd.text)
+        # get data of all tables 
+        records = []
+        for t in tables:
+                tbody = t.find('tbody')
+                rows = list(tbody.find_all('tr'))
+                for r in rows:
+                        tds = r.find_all('td')
+                        re = []
+                        for td in tds:
+                                re.append(td.text)
+                        if len(re) == len(head):
+                                records.append(re)
+        # build dictionary of month to number
+        mon = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        d = dict(zip(mon, range(1, 13)))
+        # split month and year, store in two row, change records data from string to int type
+        for i in range(len(records)):
+             tmp = []
+             tmp.append(d[records[i][0][:3]])
+             tmp.append(int(records[i][0][-2:]))
+             for j in range(2, len(records[0])):
+                     tmp.append(int(records[i][j].replace(',', '')))
+             records[i] = tmp
+        # modify head
+        head = ['Month', 'Year'] + head[1:]
+        # change records to chronological order
+        l = [records[-1]]
+        tmp = records[-1][1]
+        re = []
+        for r in records[::-1][1:]:
+                if l[-1][1] == r[1]:
+                        l.append(r)
+                else:
+                        re += l[::-1]
+                        l = [r]
+                        tmp = r[1]   
+        re += l[::-1]
+        return [head] + re
 
-def cleandata():
-        def helper(data):
-                res = data[:4]
-                for i in range(4, len(data), 12):
-                        res = data[i:i+12] + res
-                return res
-
-        def toint(data):
-                for i in range(len(data)):
-                        data[i] = int(data[i].replace(',', ''))
-                return data
-
-        months = []
-        db = []
-        fcc = []
-        fcs = []
-        for r in records:
-                months.append(r[0])
-                db.append(r[1])
-                fcc.append(r[2])
-                fcs.append(r[3])
-        mth = helper(months)
-        deb = toint(helper(db))
-        cash = toint(helper(fcc))
-        sec = toint(helper(fcs))
-        return (mth, deb, cash, sec)
-
-def plot(mth, deb, cash, sec):
-        fig, ax =plt.subplots(figsize = (125, 7))
-        ax.plot(mth, deb, 'ro', label = "Debit Balances in Customers' 'Securities Margin Accounts")
-        ax.plot(mth, cash, 'g^', label = "Free Credit Balance in Customers Cash Accounts")
-        ax.plot(mth, sec, 'b+', label = "Free Credit Balenca in Customers' Securities Margin Accounts")
-        ax.xaxis.set_tick_params(rotation=70)
-        ax.xaxis.set_major_locator(plt.MaxNLocator(14))
-        plt.ylabel('in $millions')
-        plt.xlabel('Year')
-        plt.title('Margin Statistics')
-        plt.legend()
-
-
-
-def output(records):
-    with open('output.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(records)
+def output(filename, records):
+        with open(filename, 'w', newline = '') as f:
+                writer = csv.writer(f)
+                writer.writerows(records)
 
 url = 'http://www.finra.org/investors/margin-statistics'
-records = scrape_wensite(url)
-output(records)
+records = getdata(url)
+output('output.csv', records)
