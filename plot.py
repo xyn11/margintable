42c27f2c0116861ba7301c75039bb5ea5626febc
xyn11 <xuyinan11@gmail.com>
2019-06-13 23:51:52 -0700

    -add right plot.py

--- plot.py
+++ plot.py
@@ -1,32 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def readdata(filename):
+def plot(filename):
+        # get dataframe
         records = pd.read_csv(filename)
+        df = pd.DataFrame(records)
+        # concatenate month and year, sum two credit accounts
+        df['Month/Year'] = df['Month'].map(str) + '/' + df['Year'].map(str)
+        df['SumCredit'] = df["Free Credit Balances in Customers' Cash Accounts"]\
+                         + df["Free Credit Balances in Customers' Securities Margin Accounts"]
+        # plot
+        ax = df.plot(x = 'Month/Year', y = ["Debit Balances in Customers' Securities Margin Accounts",\
+                         "Free Credit Balances in Customers' Cash Accounts",\
+                         "Free Credit Balances in Customers' Securities Margin Accounts", "SumCredit"],\
+                          style = ['ro', 'g^', 'b+', 'y*'], figsize = (25, 7))
+        ax.set_ylabel('in millions')
+        ax.set_title('Margin Statistics')
+        fig = ax.get_figure()
+        fig.savefig('plot.png')
 
-        months = []
-        db = []
-        fcc = []
-        fcs = []
-        for r in records:
-                months.append(r[0])
-                db.append(r[1])
-                fcc.append(r[2])
-                fcs.append(r[3])
-        mth = helper(months)
-        deb = toint(helper(db))
-        cash = toint(helper(fcc))
-        sec = toint(helper(fcs))
-        return (mth, deb, cash, sec)
-
-def plot(mth, deb, cash, sec):
-        fig, ax =plt.subplots(figsize = (125, 7))
-        ax.plot(mth, deb, 'ro', label = "Debit Balances in Customers' Securities Margin Accounts")
-        ax.plot(mth, cash, 'g^', label = "Free Credit Balance in Customers' Cash Accounts")
-        ax.plot(mth, sec, 'b+', label = "Free Credit Balenca in Customers' Securities Margin Accounts")
-        ax.xaxis.set_tick_params(rotation=70)
-        ax.xaxis.(plt.MaxNLocator(14))
-        plt.ylabel('in $millions')
-        plt.xlabel('Month-Year')
-        plt.title('Margin Statistics')
-        plt.legend()
+plot('output.csv')
